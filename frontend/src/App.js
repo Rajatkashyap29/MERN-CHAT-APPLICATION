4bc8383a4b3f5419243924e475516a99fe0ef516
Rajatkashyap29 <mr.rajat.29@gmail.com>
2025-12-15 20:06:35 +0530

    Fix: remove frontend submodule and add as normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import Homepage from './pages/Homepage';
+import Chatpage from './pages/Chatpage'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+       <Route path='/' Component={Homepage} />
+       <Route path="/chats" Component={Chatpage}/>
+      </Routes> 
+    </div>
+  );
+}
+
+export default App;
+
+
+
